359dd48f70592ae89d04de4c210be08eccd434b3
Vitaly Batrakov <batrakov.vitaly@gmail.com>
2018-02-03 19:35:34 +0700

    Add FeedErrorTableViewCell

---
+++ RSSReader/RSSReader/PresentationLayer/Modules/FeedList/Cell/FeedErrorTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  FeedErrorTableViewCell.h
+//  RSSReader
+//
+//  Created by Vitaly Batrakov on 03.02.2018.
+//  Copyright © 2018 vbat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FeedErrorTableViewCell : UITableViewCell
+
+@end
---
+++ RSSReader/RSSReader/PresentationLayer/Modules/FeedList/Cell/FeedErrorTableViewCell.m
@@ -0,0 +1,21 @@
+//
+//  FeedErrorTableViewCell.m
+//  RSSReader
+//
+//  Created by Vitaly Batrakov on 03.02.2018.
+//  Copyright © 2018 vbat. All rights reserved.
+//
+
+#import "FeedErrorTableViewCell.h"
+
+@implementation FeedErrorTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+}
+
+@end
